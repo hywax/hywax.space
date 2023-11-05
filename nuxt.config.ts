859a84a5d9c2085bbddf55c976c71f9083da7165
hywax <a.hywax@gmail.com>
2023-11-05 20:06:33 +0500

    Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,49 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true
+  },
   modules: [
-    '@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@nuxt/content'
-  ]
+    '@nuxtjs/i18n',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-phosphor-icons'
+  ],
+  tailwindcss: {
+    cssPath: '~/assets/style/tailwind.scss'
+  },
+  colorMode: {
+    classSuffix: ''
+  },
+  content: {
+    locales: [
+      // 'en',
+      'ru'
+    ],
+    defaultLocale: 'ru',
+    navigation: {
+      fields: ['publishedAt']
+    }
+  },
+  i18n: {
+    langDir: 'locales',
+    strategy: 'prefix_except_default',
+    defaultLocale: 'ru',
+    locales: [
+      // {
+      //  code: 'en',
+      //  iso: 'en-US',
+      //  name: 'English',
+      //  file: 'en-US.json'
+      // },
+      {
+        code: 'ru',
+        iso: 'ru-RU',
+        name: 'Русский',
+        file: 'ru-RU.json'
+      }
+    ]
+  }
 })
