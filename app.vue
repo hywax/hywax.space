aee2b37bbdabe8cb5d3fec782a688b41a871f5e0
hywax <a.hywax@gmail.com>
2023-11-05 19:19:52 +0500

    i18n append to head meta fields

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  const i18nHead = useLocaleHead({
+    addSeoAttributes: {
+      canonicalQueries: []
+    }
+  })
+
+  useHead({
+    htmlAttrs: {
+      lang: i18nHead.value.htmlAttrs!.lang
+    },
+    link: [...(i18nHead.value.link || [])],
+    meta: [...(i18nHead.value.meta || [])]
+  })
+</script>
