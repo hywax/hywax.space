92cbc56555c52750f87844d2d8798f46f9a9a1ba
hywax <a.hywax@gmail.com>
2023-11-08 11:16:49 +0500

    Add not found block component

---
+++ components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="py-4 rounded-md border-dashed border-2 border-surface-hover text-fg-less-dimmed text-center font-medium"
+  >
+    {{ $t('not-found') }}
+  </div>
+</template>
