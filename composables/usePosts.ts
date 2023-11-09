d2321f46ab58ce954bf7193b211b0591b5ef04d7
hywax <a.hywax@gmail.com>
2023-11-09 12:21:36 +0500

    Create usePosts

---
+++ composables/usePosts.ts
@@ -0,0 +1,42 @@
+import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
+import { useLocale } from '~/composables/useLocale'
+
+export interface PostsItem {
+  name: string
+  link: string
+  readingTime: string
+  published: string
+}
+
+export interface PostsContent extends MarkdownParsedContent {
+  publishedAt: string
+}
+
+export const usePosts = () => {
+  const { locale } = useLocale()
+
+  const formatDate = (date: string) => new Date(date).toLocaleDateString(locale.value)
+
+  const getPosts = async (limit = 0): Promise<PostsItem[]> => {
+    const list = await queryContent<PostsContent>('posts')
+      .where({
+        publishedAt: {
+          $exists: true
+        }
+      })
+      .limit(limit)
+      .find()
+
+    return list.map((content) => ({
+      name: content.title!,
+      link: content._path!,
+      readingTime: Math.ceil(content.readingTime.minutes).toString(),
+      published: formatDate(content.publishedAt)
+    }))
+  }
+
+  return {
+    getPosts,
+    formatDate
+  }
+}
