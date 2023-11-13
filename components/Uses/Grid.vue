f514d0e752d09cd086935e4c7dc9864459116179
hywax <a.hywax@gmail.com>
2023-11-13 10:40:25 +0500

    Refactor to `NotFound` component

--- components/Uses/Grid.vue
+++ components/Uses/Grid.vue
@@ -2,12 +2,7 @@
   <div v-if="items.length" class="grid grid-cols-1 md:grid-cols-2 gap-2">
     <UsesCard v-for="(item, key) in items" :key="key" :use="item" />
   </div>
-  <div
-    v-else
-    class="py-4 rounded-md border-dashed border-2 border-surface-hover text-fg-less-dimmed text-center font-medium"
-  >
-    {{ $t('not-found') }}
-  </div>
+  <NotFound v-else />
 </template>
 
 <script setup lang="ts">
