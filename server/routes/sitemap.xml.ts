22ed869e246af6238d06d056667a34e4b42095f1
hywax <a.hywax@gmail.com>
2023-11-14 15:57:42 +0500

    Implements sitemap handler

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,20 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { serverQueryContent } from '#content/server'
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const docs = await serverQueryContent(event).find()
+  const sitemap = new SitemapStream({
+    hostname: config.host.url
+  })
+
+  for (const doc of docs) {
+    sitemap.write({
+      url: doc._path,
+      changefreq: 'monthly'
+    })
+  }
+  sitemap.end()
+
+  return streamToPromise(sitemap)
+})
