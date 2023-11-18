8ab395872fe24454a518bd43000c346ab10d02b4
hywax <a.hywax@gmail.com>
2023-11-18 21:00:14 +0500

    feat: add supports external link

--- components/Projects/Card.vue
+++ components/Projects/Card.vue
@@ -1,19 +1,14 @@
 <template>
-  <a
-    class="flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20"
-    target="_blank"
-    rel="noreferrer nofollow"
-    :href="project.link"
-  >
-    <div class="flex flex-col gap-y-2">
+  <a v-bind="attrs">
+    <div class="flex flex-col gap-y-2 h-full">
       <h3 class="font-bold text-xl">
         {{ project.name }}
       </h3>
-      <p class="text-fg-dimmed leading-snug">
+      <p class="text-fg-dimmed leading-snug mb-8">
         {{ project.description }}
       </p>
 
-      <Component :is="icon" class="lock w-5 h-5 mt-8 self-end text-current" />
+      <Component :is="icon" class="lock w-5 h-5 mt-auto self-end text-current" />
     </div>
   </a>
 </template>
@@ -36,5 +31,22 @@
     }
 
     return PhosphorIconLink
+  })
+
+  const isExternalUrl = (string) => {
+    try {
+      return Boolean(new URL(string))
+    } catch (e) {}
+
+    return false
+  }
+
+  const attrs = computed(() => {
+    return {
+      href: props.project.link,
+      class:
+        'flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20',
+      ...(isExternalUrl(props.project.link) ? { target: '_blank', rel: 'noreferrer nofollow' } : null)
+    }
   })
 </script>
