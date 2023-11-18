f61cbb827b81ad5dd6ac2e73332308ae9220ea1f
hywax <a.hywax@gmail.com>
2023-11-18 21:04:28 +0500

    feat: add muuto page

---
+++ content/ru/projects/muuto.md
@@ -0,0 +1,12 @@
+---
+title: 'Muuto'
+description: 'Небольшой генератор слов на языке IKEA. Идеально подходит для генерации названий домашних проектов.'
+---
+
+::h1{.title}
+{{ $doc.title }}
+::
+
+::div{.subtitle.mb-16}
+{{ $doc.description }}
+::
---
+++ pages/projects/muuto.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="mx-auto w-full max-w-[80ch]">
+    <ContentDoc v-slot="{ doc }" :query="query">
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+
+    <div class="rounded-lg p-6 border border-surface hover:border-surface-hover transition-colors">
+      <TransitionGroup tag="ul" name="fade" class="flex flex-wrap gap-2 leading-8 text-xl mb-8">
+        <li v-for="(word, key) in words" :key="key">
+          {{ word }}
+        </li>
+      </TransitionGroup>
+
+      <div class="flex gap-x-4 justify-end">
+        <button class="text-lg" @click="roll">
+          <PhosphorIconArrowsClockwise />
+          <span>{{ $t('projects.reload') }}</span>
+        </button>
+        <a
+          href="https://github.com/hywax/muuto"
+          class="text-lg buttonish"
+          target="_blank"
+          rel="noreferrer nofollow"
+        >
+          <PhosphorIconGithubLogo />
+          <span>{{ $t('projects.code') }}</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import muuto from 'muuto'
+
+  const { query } = useLocaleContent()
+
+  const words = ref<string[]>([])
+
+  const getWords = () => Array.from({ length: 70 }, () => muuto())
+
+  const roll = () => {
+    words.value = [...getWords()]
+  }
+
+  onMounted(() => {
+    roll()
+  })
+</script>
+
+<style lang="scss" scoped>
+  .fade-move,
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    transform: scaleY(0.01) translate(30px, 0);
+  }
+
+  .fade-leave-active {
+    position: absolute;
+  }
+</style>
