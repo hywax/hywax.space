135679740e717ee6bba6e17bada51cd4ee369542
hywax <a.hywax@gmail.com>
2024-01-13 19:53:28 +0500

    feat: social licks in content component

---
+++ components/content/Social.vue
@@ -0,0 +1,7 @@
+<template>
+  <p flex="~ gap-3 wrap" class="mt--2!">
+    <a href="#" target="_blank"><span class="i-uil-github-alt" /> GitHub</a>
+    <a href="#" target="_blank"><span class="i-ri-twitter-x-fill" /> Twitter</a>
+    <a href="#" target="_blank"><span class="i-la-telegram-plane" /> Telegram</a>
+  </p>
+</template>
