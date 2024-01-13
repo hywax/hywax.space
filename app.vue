10f7617bbb8d47d7de9578c506113ad4c7062da5
hywax <a.hywax@gmail.com>
2024-01-13 23:03:00 +0500

    chore: style app

--- app.vue
+++ app.vue
@@ -17,5 +17,14 @@
     },
     link: [...(i18nHead.value.link || [])],
     meta: [...(i18nHead.value.meta || [])],
+    bodyAttrs: {
+      class: 'font-sans text-gray-700 dark:text-gray-200 relative',
+    },
   })
 </script>
+
+<style>
+  @import '@unocss/reset/tailwind.css';
+  @import '~/assets/style/main.css';
+  @import '~/assets/style/prose.css';
+</style>
