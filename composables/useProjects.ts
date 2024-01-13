ff27436107ea1fb07d659e4a1b9269fc7c3f5e35
hywax <a.hywax@gmail.com>
2024-01-13 22:51:06 +0500

    refactor: useProjects

--- composables/useProjects.ts
+++ composables/useProjects.ts
@@ -1,32 +1,55 @@
-import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
-
-export interface ProjectsItem {
-  name: string
-  description: string
-  link: string
-  featured?: boolean
-}
-
-export interface ProjectsContent extends MarkdownParsedContent {
-  projects: ProjectsItem[]
-}
-
-export interface ContributionsItem {
-  name: string
-  url: string
-  star: number
-}
+import type { ContributionsItem, ProjectsContent, ProjectsItem } from '~/types'
 
 export function useProjects() {
-  const { locale } = useLocale()
-  const getProjects = async (limit = 0): Promise<ProjectsItem[]> => {
+  const { locale } = useI18n()
+  const getProjects = async (): Promise<ProjectsItem[]> => {
     const page = await queryContent<ProjectsContent>('projects').where({ _locale: locale.value }).findOne()
 
-    return limit ? page.projects.slice(0, limit) : page.projects
+    return page.projects
   }
 
   const getContributions = async () => {
-    const { data } = await useFetch<ContributionsItem[]>('/api/contributions')
+    const json = [
+      {
+        name: 'NginxProxyManager/nginx-proxy-manager',
+        url: 'https://github.com/NginxProxyManager/nginx-proxy-manager',
+        star: 17161,
+      },
+      {
+        name: 'vitejs/awesome-vite',
+        url: 'https://github.com/vitejs/awesome-vite',
+        star: 12171,
+      },
+      {
+        name: 'offen/docker-volume-backup',
+        url: 'https://github.com/offen/docker-volume-backup',
+        star: 1262,
+      },
+      {
+        name: 'nuxt/modules',
+        url: 'https://github.com/nuxt/modules',
+        star: 839,
+      },
+      {
+        name: 'vitejs/vite-plugin-vue',
+        url: 'https://github.com/vitejs/vite-plugin-vue',
+        star: 357,
+      },
+      {
+        name: 'alteracs/html-converter',
+        url: 'https://github.com/alteracs/html-converter',
+        star: 0,
+      },
+      {
+        name: 'copity/copity',
+        url: 'https://github.com/copity/copity',
+        star: 0,
+      },
+    ]
+
+    return json
+
+    const { data } = await useFetch<ContributionsItem[]>('/contributions')
 
     return data
   }
