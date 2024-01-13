07212a14093c4bea2eb6d000e7595b47db3799f0
hywax <a.hywax@gmail.com>
2024-01-13 20:01:02 +0500

    refactor: useLocale -> useI18n

--- composables/useLocaleContent.ts
+++ composables/useLocaleContent.ts
@@ -1,7 +1,7 @@
 import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
 
 export function useLocaleContent() {
-  const { locale } = useLocale()
+  const { locale } = useI18n()
   const route = useRoute()
 
   const query: QueryBuilderParams = computed(() => ({
