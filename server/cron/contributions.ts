447f4008f573c03a8f00c2bf091828713edef8c2
hywax <a.hywax@gmail.com>
2024-01-13 22:49:05 +0500

    refactor: useContributions

--- server/cron/contributions.ts
+++
@@ -1,6 +0,0 @@
-import { defineCronHandler } from '#nuxt/cron'
-import { collectContributions } from '~/server/services/contributions'
-
-export default defineCronHandler('daily', () => {
-  return collectContributions()
-})
--- server/routes/api/contributions.ts
+++
@@ -1,5 +0,0 @@
-import { getContributions } from '~/server/services/contributions'
-
-export default defineEventHandler(async () => {
-  return await getContributions()
-})
