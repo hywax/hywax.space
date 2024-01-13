50ee3764dcdfd77070e2748ec3fed1f9f5e67f83
hywax <a.hywax@gmail.com>
2024-01-13 19:52:03 +0500

    refactor: new style lang switcher

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,32 +1,27 @@
 <template>
-  <div v-if="Object.values(locales).length" class="relative inline-block text-left">
-    <button
-      ref="trigger"
-      class="p-1 rounded-sm font-semibold transition bg-transparent hover:bg-transparent link"
-      @click="visible = !visible"
-    >
-      <PhosphorIconTranslate class="block w-6 h-6" />
+  <div class="relative self-center">
+    <button ref="trigger" class="op60 hover:op100 transition" :aria-label="$t('langSwitcher.change')" @click="visible = !visible">
+      <div class="i-ph-translate" />
     </button>
 
     <div
+      v-show="visible"
       ref="dropdown"
       role="menu"
-      class="absolute left-0 md:right-0 md:left-auto z-10 mt-2 w-36 origin-top-right rounded-md bg-white dark:bg-surface shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+      class="dropdown absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md"
       aria-orientation="vertical"
       aria-labelledby="menu-button"
-      :style="`display: ${visible ? 'block' : 'none'}`"
     >
-      <div class="py-1" role="none">
-        <button
-          v-for="locale in locales"
-          :key="locale.code"
-          :disabled="locale.current"
+      <div class="py-1">
+        <NuxtLink
+          v-for="loc in locales"
+          :key="loc.code"
+          :to="switchLocalePath(loc.code)"
           role="menuitem"
-          class="block w-full rounded-sm px-4 py-2 text-left transition hover:text-accent"
-          @click.prevent="changeLocale(locale.code)"
+          class="item block w-full px-4 py-2 text-left text-sm transition"
         >
-          {{ locale?.name }}
-        </button>
+          {{ loc?.name }}
+        </NuxtLink>
       </div>
     </div>
   </div>
@@ -34,21 +29,32 @@
 
 <script setup lang="ts">
   import { onClickOutside } from '@vueuse/core'
-  import { useLocale } from '~/composables/useLocale'
 
-  const { locales, changeLocale } = useLocale()
+  const { locales } = useI18n()
 
   const trigger = ref<HTMLButtonElement>()
   const dropdown = ref<HTMLDivElement>()
+
   const visible = ref(false)
 
-  onClickOutside(
-    dropdown,
-    () => {
-      if (visible.value === true) {
-        visible.value = false
-      }
-    },
-    { ignore: [trigger] },
-  )
+  onClickOutside(dropdown, () => {
+    if (visible.value) {
+      visible.value = false
+    }
+  }, { ignore: [trigger] })
 </script>
+
+<style scoped>
+  .dropdown {
+    background-color: var(--surface);
+  }
+
+  .item {
+    opacity: .5;
+  }
+
+  .item:hover {
+    opacity: 1;
+    background-color: var(--surface-hover);
+  }
+</style>
