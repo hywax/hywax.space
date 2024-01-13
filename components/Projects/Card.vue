9e23380e7a83b6d190f1247e811bd5c2ad78fbe6
hywax <a.hywax@gmail.com>
2024-01-13 23:00:36 +0500

    refactor: new ui projects

--- components/Projects/Card.vue
+++ components/Projects/Card.vue
@@ -1,52 +1,37 @@
 <template>
-  <a v-bind="attrs">
-    <div class="flex flex-col gap-y-2 h-full">
-      <h3 class="font-bold text-xl">
+  <a href="#" class="project relative flex items-center op70 hover:op100 transition">
+    <div class="pt-2 pr-4">
+      <div class="w-9 h-9 opacity-50 i-uil-github-alt" />
+    </div>
+    <div class="flex-auto">
+      <div class="text-normal">
         {{ project.name }}
-      </h3>
-      <p class="text-fg-dimmed leading-snug mb-8">
+      </div>
+      <div class="text-sm opacity-50 font-normal">
         {{ project.description }}
-      </p>
-
-      <Component :is="icon" class="lock w-5 h-5 mt-auto self-end text-current" />
+      </div>
     </div>
   </a>
 </template>
 
 <script setup lang="ts">
-  import type { ProjectsItem } from '~/pages/projects.vue'
-  import { PhosphorIconGithubLogo, PhosphorIconLink } from '#components'
+  import type { ProjectsItem } from '~/types'
 
   export interface Props {
     project: ProjectsItem
   }
 
-  const props = defineProps<Props>()
+  defineProps<Props>()
+</script>
 
-  const icon = computed(() => {
-    const { link } = props.project
-
-    if (link.includes('github')) {
-      return PhosphorIconGithubLogo
-    }
-
-    return PhosphorIconLink
-  })
-
-  function isExternalUrl(string) {
-    try {
-      return Boolean(new URL(string))
-    } catch (e) {}
-
-    return false
+<style scoped>
+  .project {
+    padding: 0.5rem 0.875rem 0.875rem;
+    border-radius: 6px;
+    font-size: 1.1rem;
   }
 
-  const attrs = computed(() => {
-    return {
-      href: props.project.link,
-      class:
-        'flex flex-col justify-between p-6 rounded-lg transition-all bg-surface hover:bg-surface-hover shadow-md shadow-surface/20',
-      ...(isExternalUrl(props.project.link) ? { target: '_blank', rel: 'noreferrer nofollow' } : null),
-    }
-  })
-</script>
+  .project:hover {
+    background-color: var(--surface-hover);
+  }
+</style>
--- components/Projects/Grid.vue
+++ components/Projects/Grid.vue
@@ -1,27 +1,24 @@
 <template>
-  <div v-if="items.length" class="mb-12 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
-    <ProjectsCard v-for="(item, key) in items" :key="key" :project="item" class="h-full" />
-
-    <NuxtLink
-      v-if="moreLink"
-      :to="moreLink"
-      class="grid place-content-center w-full h-full py-4 rounded-md border-dashed border-2 border-surface hover:border-surface-hover transition"
-    >
-      <span class="text-fg-less-dimmed font-medium">{{ $t('more') }} →</span>
-    </NuxtLink>
+  <div class="mt-12">
+    <h2 class="mb-8 font-bold text-center text-lg op75">
+      {{ title }}
+    </h2>
+    <div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+      <ProjectsCard
+        v-for="(item, key) in items"
+        :key="key"
+        :project="item"
+      />
+    </div>
   </div>
-  <NotFound v-else>
-    {{ $t('projects.not-found') }}
-  </NotFound>
 </template>
 
 <script setup lang="ts">
-  import type { RouteLocationRaw } from 'vue-router'
-  import type { ProjectsItem } from '~/composables/useProjects'
+  import type { ProjectsItem } from '~/types'
 
   export interface Props {
+    title: string
     items: ProjectsItem[]
-    moreLink?: RouteLocationRaw
   }
 
   defineProps<Props>()
