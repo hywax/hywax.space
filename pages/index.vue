019c2e66ad7b38035a24f48d30f724e4857d3287
hywax <a.hywax@gmail.com>
2024-01-13 23:00:09 +0500

    refactor: new ui index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,9 @@
 <template>
-  <article class="prose enhanced-prose">
+  <article class="prose m-a">
     <ContentDoc :query="query" />
-
-    <h2 class="text-3xl">
-      {{ $t('blog') }}
-    </h2>
-    <div class="not-prose">
-      <PostsGrid :items="posts" :more-link="posts.length > 6 ? localePath({ name: 'posts' }) : null" />
-    </div>
-
-    <h2 class="text-3xl">
-      {{ $t('projects') }}
-    </h2>
-    <div class="not-prose">
-      <ProjectsGrid
-        :items="projects"
-        :more-link="projects.length > 6 ? localePath({ name: 'projects' }) : null"
-      />
-    </div>
   </article>
 </template>
 
 <script setup lang="ts">
-  import { useProjects } from '~/composables/useProjects'
-  import { usePosts } from '~/composables/usePosts'
-
   const { query } = useLocaleContent()
-
-  const { getProjects } = useProjects()
-  const projects = await getProjects(7)
-
-  const { getPosts } = usePosts()
-  const posts = await getPosts(7)
 </script>
