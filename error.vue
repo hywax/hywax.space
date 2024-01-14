fd61d6d3bbf7af5ba2a48a247501c5940c5c7215
hywax <a.hywax@gmail.com>
2024-01-14 17:33:32 +0500

    feat: error page

---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout>
+    <div class="prose m-a">
+      <h1>{{ error.statusCode }}</h1>
+      <p>{{ error.message }}</p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    error: {
+      url: string
+      statusCode: number
+      statusMessage: string
+      message: string
+      stack: string
+      data: Record<string, any>
+    }
+  }>()
+</script>
