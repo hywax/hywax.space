6b4d36a1fb646a43980f84a1878a294cdb958650
hywax <a.hywax@gmail.com>
2024-01-14 16:25:22 +0500

    chore: move social links to app config

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  social: {
+    github: 'https://github.com/hywax',
+    twitter: 'https://twitter.com/hywax_',
+    telegram: 'https://t.me/hywax',
+  },
+})
--- components/content/Social.vue
+++ components/content/Social.vue
@@ -1,11 +1,11 @@
 <template>
   <p flex="~ gap-3 wrap" class="mt--2!">
-    <a :href="config.social?.github" rel="nofollow" target="_blank"><span class="i-uil-github-alt" /> GitHub</a>
-    <a :href="config.social?.twitter" rel="nofollow" target="_blank"><span class="i-ri-twitter-x-fill" /> Twitter</a>
-    <a :href="config.social?.telegram" rel="nofollow" target="_blank"><span class="i-la-telegram-plane" /> Telegram</a>
+    <a :href="social.github" rel="nofollow" target="_blank"><span class="i-uil-github-alt" /> GitHub</a>
+    <a :href="social.twitter" rel="nofollow" target="_blank"><span class="i-ri-twitter-x-fill" /> Twitter</a>
+    <a :href="social.telegram" rel="nofollow" target="_blank"><span class="i-la-telegram-plane" /> Telegram</a>
   </p>
 </template>
 
 <script setup lang="ts">
-  const config = useRuntimeConfig()
+  const { social } = useAppConfig()
 </script>
