f63ab50f6f109eeed6502ad93650ef43a3b4af91
hywax <a.hywax@gmail.com>
2024-01-14 15:10:35 +0500

    chore: ProseH4 override

---
+++ components/content/ProseH4.vue
@@ -0,0 +1,22 @@
+<template>
+  <h4 :id="id">
+    <slot />
+    <a
+      v-if="id && generate"
+      :href="`#${id}`"
+      class="anchor"
+    >
+      #
+    </a>
+    <slot v-else />
+  </h4>
+</template>
+
+<script setup lang="ts">
+  import { computed, useRuntimeConfig } from '#imports'
+
+  const props = defineProps<{ id?: string }>()
+
+  const { headings } = useRuntimeConfig().public.mdc
+  const generate = computed(() => props.id && headings?.anchorLinks?.h4)
+</script>
