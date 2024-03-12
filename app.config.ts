0ab136b0d6b50a3208e4538517d46bf6adaa1058
hywax <me@hywax.space>
2024-03-12 21:47:24 +0500

    chore: remove X social

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,6 @@ export default defineAppConfig({
   authorName: 'Hywax',
   social: {
     github: 'https://github.com/hywax',
-    twitter: 'https://twitter.com/hywax_',
     telegram: 'https://t.me/hywax',
   },
   licences: {
--- components/content/Social.vue
+++ components/content/Social.vue
@@ -1,7 +1,6 @@
 <template>
   <p flex="~ gap-3 wrap" class="mt--2!">
     <a :href="social.github" rel="nofollow" target="_blank"><span class="i-uil-github-alt" /> GitHub</a>
-    <a :href="social.twitter" rel="nofollow" target="_blank"><span class="i-ri-twitter-x-fill" /> Twitter</a>
     <a :href="social.telegram" rel="nofollow" target="_blank"><span class="i-la-telegram-plane" /> Telegram</a>
   </p>
 </template>
